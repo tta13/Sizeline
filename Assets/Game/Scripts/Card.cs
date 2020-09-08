ca17d6ed356d0c51d178b613ad8c1d802f609d45
tta13 <tta@cin.ufpe.br>
2020-09-08 18:11:47 -0300

    Creating cards

--- Assets/Game/Scripts/Card.cs
+++ Assets/Game/Scripts/Card.cs
@@ -3,6 +3,7 @@
 [CreateAssetMenu(fileName = "New card", menuName = "Card")]
 public class Card : ScriptableObject
 {
+    public new string name;
     public Sprite sprite;
     public float size;
 }
